faacd7294346409616f4870e7c83b7a223928553
Tatsukiz <tatsukix0206@gmail.com>
2019-04-19 03:14:14 +0900

    オブジェクト指向で簡易なRPGゲームを作成。

---
+++ scala-grammar/src/main/scala/RPG.scala
@@ -0,0 +1,38 @@
+import java.util.Random
+
+object RPG {
+  val random = new Random
+
+}
+
+abstract class Creature(var hitPoint: Int, var attackDamage: Int) {
+  def isAlive(): Boolean = this.hitPoint > 0
+}
+
+class Hero(_hitPoint: Int, _attackDamage: Int) extends Creature(_hitPoint, _attackDamage) {
+
+  def attack(monster: Monster): Unit = {
+    monster.hitPoint = monster.hitPoint - this.attackDamage
+    this.hitPoint = this.hitPoint - monster.attackDamage
+  }
+
+  def escape(monster: Monster): Boolean = {
+    val isEscaped = RPG.random.nextInt(2) == 1
+    if(!isEscaped) {
+      this.hitPoint = this.hitPoint - monster.attackDamage
+    } else {
+      monster.isAwayFromHero = true
+    }
+    isEscaped
+  }
+
+  override def toString = s"Hero(体力:${hitPoint}, 攻撃力:${attackDamage})"
+
+}
+
+class Monster(_hitPoint: Int, _attackDamage: Int, var isAwayFromHero: Boolean)
+  extends  Creature(_hitPoint, _attackDamage) {
+
+  override def toString = s"Monster(体力:${hitPoint}, 攻撃力:${attackDamage}, ヒーローから離れている:${isAwayFromHero})"
+
+}
