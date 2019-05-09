cd605f28b4b02725874c01061af6ee33c18333fc
Tatsukiz <tatsukix0206@gmail.com>
2019-05-10 02:54:19 +0900

    リスト・配列・レンジを練習

---
+++ scala-fp/src/main/scala/Collection.scala
@@ -0,0 +1,21 @@
+// Seq[Int] = List(1, 2, 3)
+val seq = Seq(1, 2, 3)
+
+// List[Int] = List(1, 2, 3)
+val list = List(1, 2, 3)
+
+// scala.collection.immutable.Nil.type = List()
+Nil
+
+// List[Int] = List(3)
+3::Nil
+
+// List[Int] = List(1, 2, 3)
+1::2::3::Nil
+
+// scala.collection.immutable.Range.Inclusive = Range 1 to 5
+1 to 5
+
+// List[Int] = List(1, 2, 3, 4, 5)
+(1 to 5).toList
+
