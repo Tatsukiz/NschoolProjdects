52e9f6d0c0b37d993759497c02f66c86f264974f
Tatsukiz <tatsukix0206@gmail.com>
2019-06-04 23:17:12 +0900

    AnyRef型を受け取るよう修正

--- scala-fp/src/main/scala/UnapplyStudy.scala
+++ scala-fp/src/main/scala/UnapplyStudy.scala
@@ -1,9 +1,25 @@
+import scala.util.Try
+
 object UnapplyStudy extends App{
 
   class User(private val name: String, private val age: Int)
 
   object User {
-    def unapply(user: User): Option[(String, Int)] = Some(user.name, user.age)
+    def unapply(obj: Any): Option[(String, Int)] =
+      if (obj.isInstanceOf[User]) {
+        val user = obj.asInstanceOf[User]
+        Some((user.name, user.age))
+      } else if (obj.isInstanceOf[String]) {
+        val strs = obj.asInstanceOf[String].split("@")
+        val name = strs.headOption
+        val age = Try { strs.tail.headOption.map(_.toInt) }.toOption.flatten
+        (name, age) match {
+          case (Some(n), Some(a)) => Some((n, a))
+          case _ => None
+        }
+      } else {
+        None
+      }
   }
 
   def printPatternMatched(obj: AnyRef): Unit = {
@@ -14,4 +30,5 @@ object UnapplyStudy extends App{
   }
 
   printPatternMatched(new User("Taro", 17))
+  printPatternMatched("Jiro@16")
 }
